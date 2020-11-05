2ec3ed4e9e0d2ed87e9f1c202ca9aab6a7e83cd0
Karolina Och-Pudło <okaa93@gmail.com>
2020-11-06 00:53:42 +0100

    add tech and footer

---
+++ app/js/carousel.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $('.tech__carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    arrows: false,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 960,
+        settings: {
+          centerMode: true,
+          slidesToShow: 4,
+          autoplay: false,
+        }
+      },
+      {
+        breakpoint: 930,
+        settings: {
+          centerMode: true,
+          slidesToShow: 3,
+          autoplay: false,
+        }
+      },
+      {
+        breakpoint: 730,
+        settings: {
+          centerMode: true,
+          slidesToShow: 2,
+          autoplay: false,
+        }
+      },
+      {
+        breakpoint: 490,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          autoplay: false,
+        }
+      }
+    ]
+    
+  });
+});
